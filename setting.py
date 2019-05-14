deadc68e7b10dda433e03956fd6abe37a8c2de75
klauswong123 <klauswangjinpeng@gmail.com>
2019-05-14 09:43:56 -0700

    initial commit

--- setting.py
+++ setting.py
@@ -1,26 +1,14 @@
 from flask import Flask
-from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from datetime import timedelta
-import random, string, hashlib
 from flask_sqlalchemy import SQLAlchemy
 import os
-import pymysql
 
 
-
-n = ''.join(random.sample(string.ascii_letters + string.digits, 32))
-m = hashlib.md5()
-m.update(n.encode('utf-8'))
-result = m.hexdigest()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://klaus:1314wang@/sgi'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SECRET_KEY"] = result
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 bootstrap = Bootstrap(app)
 
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
