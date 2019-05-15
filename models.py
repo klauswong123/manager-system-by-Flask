682138237e4beb393617f9a3c2eb015fcc18e2a8
klauswong123 <klauswangjinpeng@gmail.com>
2019-05-15 03:18:49 -0700

    make it better

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from setting import db,app
 from datetime import datetime
-
+from datetime import timedelta
 db.init_app(app)
 
 class Users(db.Model):
@@ -18,15 +18,15 @@ class Coupon(db.Model):
     key = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id = db.Column(db.String(64), unique=True)
     active_state = db.Column(db.Boolean, default=True)
-    create_date = db.Column(db.DateTime, default=datetime.now)
-    use_date = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    create_date = db.Column(db.DateTime, default=(datetime.now + timedelta(hours=8)))
+    use_date = db.Column(db.DateTime, default=(datetime.now + timedelta(hours=8)), onupdate=(datetime.now + timedelta(hours=8)))
     user_name = db.Column(db.String(64), unique=True)
     user_phone = db.Column(db.Integer, default=0)
 
 #coupon1 = Coupon(id='123hjh')
 #db.session.add(coupon1)
 #db.session.commit()
-#db.drop_all()
+db.drop_all()
 db.create_all()
 #coupon = Coupon(id='qwenqwueqw')
 #db.session.add(coupon)
